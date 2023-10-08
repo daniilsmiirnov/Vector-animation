dda5ed8ba2e62cc2583abc2aa78d30d177898bb6
daniilsmiirnov <dan_smirnov@mail.ru>
2023-10-08 13:37:20 +0300

    first animation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector("canvas");
 const cxt = canvas.getContext("2d");
 console.log(canvas)
-canvas.width= 1280;
-canvas.height= 720;
+canvas.width= 1920;
+canvas.height= 1080;
 
 function resize() {
 	canvas.width = 500;
@@ -14,15 +14,33 @@ function clearCanvas(canvas) {
 }
 
 function frame(cxt){ //Рамка
+    requestAnimationFrame(frame);
+    console.log('frame')
     cxt.beginPath();
-    cxt.rect(0,0,1280,720);
+    cxt.rect(0,0,1920,1080);
     cxt.fillStyle = 'grey';
     cxt.fill();
 
     cxt.lineWidth = 10;
     cxt.stroke();
-
 }
 
+let x=200; 
+let y=700;
+function car (){
+    requestAnimationFrame(car);
+    
+    console.log('hi');
+    cxt.beginPath();
+    cxt.rect(x,y,200,100)
+    x+=1;
+    cxt.fillStyle = 'blue';
+    cxt.fill();
+    cxt.lineWidth = 2;
+    cxt.stroke();
 
+}
 frame(cxt); //прямоугольная рамка
+requestAnimationFrame(car);
+requestAnimationFrame(frame);
+//car(cxt,x,y);
